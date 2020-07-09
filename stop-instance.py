ff92f5248eb4bd421c8c6dd735471c18a0a83199
maayrf <maay.rodrigues.f@hotmail.com>
2020-07-09 10:32:33 -0300

    my ec2 with lambda and cloudwatch

---
+++ stop-instance.py
@@ -0,0 +1,7 @@
+import boto3
+region = 'us-east-1'
+instance = ['COLOQUE O ID DA SUA INSTANCIA']
+
+def lambda_handler(event, context):
+    ec2 = boto3.client('ec2', region_name=region)
+    ec2.stop_instances(InstanceIds=instance)
